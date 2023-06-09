ab1c558497522adde59b6ce8a499e3c5bf0a9bb4
Hakimbek <hakimbekmrx@gmail.com>
2023-06-09 19:29:29 +0500

    002

---
+++ lib/pages/editing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditingPage extends StatefulWidget {
+  const EditingPage({Key? key}) : super(key: key);
+
+  @override
+  State<EditingPage> createState() => _EditingPageState();
+}
+
+class _EditingPageState extends State<EditingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

9fcaed51f489bf19bec03daaf185fb7e21fe665d
Hakimbek <hakimbekmrx@gmail.com>
2023-06-10 15:35:01 +0500

    0018

--- lib/pages/controller_page.dart
+++ lib/pages/controller_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:task2/pages/create_page.dart';
+import 'package:task2/pages/profile_page.dart';
 import 'package:task2/pages/search_page.dart';
 import 'package:task2/pages/home_page.dart';
 
@@ -13,8 +14,64 @@ class ControllerPage extends StatefulWidget {
 
 class _ControllerPageState extends State<ControllerPage> {
 
+  int currentIndex = 0;
+  PageController pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pageController,
+        onPageChanged: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        children: [
+          HomePage(),
+          SearchPage(),
+          CreatePage(),
+          ProfilePage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        currentIndex: currentIndex,
+        selectedIconTheme: IconThemeData(color: Colors.blue),
+        selectedLabelStyle: TextStyle(color: Colors.grey),
+        selectedFontSize: 0,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        unselectedFontSize: 0,
+        unselectedIconTheme: IconThemeData(color: Colors.grey),
+        unselectedLabelStyle: TextStyle(color: Colors.grey),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.search),
+              label: "Search"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.add),
+              label: "Create"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.person),
+              label: "Profile"
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget a() {
     return CupertinoTabScaffold(
       tabBar: CupertinoTabBar(
         backgroundColor: Colors.white.withOpacity(.7),
@@ -22,6 +79,7 @@ class _ControllerPageState extends State<ControllerPage> {
           BottomNavigationBarItem(icon: Icon(CupertinoIcons.home),label: "Home"),
           BottomNavigationBarItem(icon: Icon(CupertinoIcons.search),label: "Search"),
           BottomNavigationBarItem(icon: Icon(CupertinoIcons.add),label: "Create"),
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.person),label: "Profile"),
         ],
       ),
       tabBuilder: (context, index) {
@@ -32,9 +90,13 @@ class _ControllerPageState extends State<ControllerPage> {
             return const SearchPage();
           case 2:
             return const CreatePage();
+          case 3:
+            return const ProfilePage();
         }
         return const SizedBox();
       },
     );
   }
+
+
 }
